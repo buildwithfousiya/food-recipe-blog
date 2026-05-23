b89f6149d2ed22dd656367572f6de3fd400fba93
fousiya <fousiya9198@gmail.com>
2026-05-23 20:45:55 +0530

    recipe details page style changed

--- frontend/food blog app/src/components/Footer.jsx
+++ frontend/food blog app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
   return (
     <div className="footer">
-      <p>@copyright imaginary engineering</p>
+      <p>@copyright foodie diary</p>
     </div>
   )
 }
--- frontend/food blog app/src/pages/recipeDetails.jsx
+++ frontend/food blog app/src/pages/recipeDetails.jsx
@@ -1,24 +1,60 @@
 import React from 'react'
 import { FaUserCircle } from 'react-icons/fa'
-import { useLoaderData } from 'react-router-dom'
+import { BsStopwatchFill, BsArrowLeft } from 'react-icons/bs'
+import { useLoaderData, useNavigate } from 'react-router-dom'
 
 export default function RecipeDetails() {
     const recipe = useLoaderData()
-    console.log(recipe)
+    const navigate = useNavigate()
+
   return (
    <>
-    <div className='outer-container'>
-        <div className='profile'>
-            <FaUserCircle size={45} color="#00a884" />
-            <h5>{recipe.email}</h5>
+    <div className='detail-page'>
+      <div className='detail-hero'>
+        <img src={`http://localhost:5000/images/${recipe.coverImage}`} alt={recipe.title} />
+        <div className='detail-hero-overlay'></div>
+        <button className='detail-back-btn' onClick={() => navigate(-1)}>
+          <BsArrowLeft /> Back
+        </button>
+      </div>
+
+      <div className='detail-content'>
+        <div className='detail-header'>
+          <h1 className='detail-title'>{recipe.title}</h1>
+          <div className='detail-meta'>
+            <div className='detail-author'>
+              <FaUserCircle size={36} color="#00a884" />
+              <span>{recipe.email}</span>
+            </div>
+            {recipe.time && (
+              <div className='detail-time'>
+                <BsStopwatchFill />
+                <span>{recipe.time}</span>
+              </div>
+            )}
+          </div>
         </div>
-        <h3 className='title'>{recipe.title}</h3>
-        <img src={`http://localhost:5000/images/${recipe.coverImage}`} width="220px" height="200px"></img>
-        <div className='recipe-details'>
-            <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map(item=>(<li>{item}</li>))}</ul></div>
-            <div className='instructions'><h4>Instructions</h4><span>{recipe.instructions}</span></div>
+
+        <div className='detail-sections'>
+          <div className='detail-ingredients'>
+            <h2>Ingredients</h2>
+            <ul>
+              {recipe.ingredients.map((item, index) => (
+                <li key={index}>
+                  <span className='ingredient-dot'></span>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='detail-instructions'>
+            <h2>Instructions</h2>
+            <p>{recipe.instructions}</p>
+          </div>
         </div>
+      </div>
     </div>
    </>
   )
-}
+}
