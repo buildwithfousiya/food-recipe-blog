8dead35fe66562ca6751bd68c5fa0a3fc4f00411
fousiya <fousiya9198@gmail.com>
2026-05-24 21:38:38 +0530

    Database connection made proper

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ const userRoutes = require("./routes/user")
 const connectDb = require("./config/connectionDb")
 const cors = require("cors")
 
-const PORT = process.env.PORT || 3000
-connectDb()
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
@@ -16,6 +15,10 @@ app.use(express.static("public"))
 app.use("/", userRoutes)
 app.use("/recipe", recipeRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch(err => {
+    console.log("database connection failed:", err.message)
 })
