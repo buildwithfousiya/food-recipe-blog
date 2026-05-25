d8cceaaaa1baa0d76b13690a4dc07c3b2c390ee5
fousiya <fousiya9198@gmail.com>
2026-05-25 20:38:38 +0530

    done some edits

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -11,6 +11,7 @@ const userSignUp = async (req, res) => {
     if (user) {
         return res.status(400).json({ error: "Email already exists" })
     }
+
     const hashPwd = await bcrypt.hash(password, 10)
     const newUser = await User.create({
         email,
@@ -21,12 +22,10 @@ const userSignUp = async (req, res) => {
 }
 const userLogin = async (req, res) => {
     const { email, password } = req.body
-    console.log(email, password)
     if (!email || !password) {
         return res.status(400).json({ message: "All fields are required" })
     }
     let user = await User.findOne({ email })
-    console.log(user)
     if (user && await bcrypt.compare(password, user.password)) {
         let token = jwt.sign({ email, id: user._id }, process.env.SECRET_KEY)
         return res.status(200).json({ token, user })
@@ -34,8 +33,8 @@ const userLogin = async (req, res) => {
     else {
         return res.status(400).json({ error: "Invalid credentials" })
     }
-
 }
+
 const getUser = async (req, res) => {
     const user = await User.findById(req.params.id)
     res.json({ email: user.email })
